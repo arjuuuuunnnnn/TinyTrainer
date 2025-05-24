61f3d2cfe4e181fb4833df8ff6b1e18631c73067
arjuuuuunnnnn <arjunbanur27@gmail.com>
2025-05-24 22:32:32 +0530

    feat: instruction FT datasets format script done

--- main.py
+++
@@ -1,8 +0,0 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-
-pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe(messages)
